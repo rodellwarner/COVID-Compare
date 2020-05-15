b1637de99a157ed92fab59101872443f177a3f50
Rodell Warner <rodell.warner@gmail.com>
2020-05-15 17:15:07 -0500

    showGraph generating viable URL

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 const URL = "https://api.covid19api.com/live/country"
 
-const graphURL = "https://quickchart.io/chart?c={type:'line',data:{labels:["
+const graphURL = "https://quickchart.io/chart?c={type:'bar',data:{labels:["
 
 let STORE = [];
 
@@ -54,101 +54,109 @@ function createSelectElement(sortedCountries) {
     $('#countries').append(`<option value="${element.country}">${element.country}</option>`);
     $('#countries2').append(`<option value="${element.country}">${element.country}</option>`);
   });
-  }
+}
 
- 
-  function handleSubmitForm() {
+function handleSubmitForm() {
     // console.log(STORE);
     $('#selectCountryForm').submit(function() {
       event.preventDefault();
       getCovidData();
       clearGraph();
     });
-  }
+}
 
-  function getCovidData() {
+function getCovidData() {
+  let country1 = `${$("#countries").val()}`;
+  let country2 = `${$("#countries2").val()}`;
 
-    let country1 = `${$("#countries").val()}`;
-    let country2 = `${$("#countries2").val()}`;
+  let chosenObject1 = STORE.filter(object => object.country === country1);
+  let chosenObject2 = STORE.filter(object => object.country === country2);
 
+  console.log(chosenObject1);
 
-    console.log("country1", country1);
-    console.log("country2", country2);
-    console.log("STORE", STORE);
-
-
-    let chosenObject1 = STORE.filter(object => object.country === country1);
-    let chosenObject2 = STORE.filter(object => object.country === country2);
-
-    console.log(chosenObject1);
-    console.log(chosenObject2);
+  showGraph(chosenObject1, chosenObject2);
+}
 
 
-    // function identifyChosenObject(country) {
-    //   const chosenObject = STORE.filter(object => object.country === country);
-    //   console.log(chosenObject);
-    // }
-
-    // identifyChosenObject(country1);
-    // identifyChosenObject(country2);
 
 
+function showGraph(object1, object2) {
+
+  let numbersDeceased = [object1[0].deceased, object2[0].deceased];
+  console.log('numbersDeceased', numbersDeceased);
+
+  let numbersInfected = [object1[0].infected, object2[0].infected];
+  console.log('numbersInfected', numbersInfected)
+
+  let countryNames = ['\'' + object1[0].country + '\'', '\'' + object2[0].country + '\''];
+  console.log('countrynames', countryNames);
+
+  console.log('country1', object1[0].country);
+  
+
+  let imageURL = `${graphURL + countryNames}` + '],datasets:[{label:\'Infected\',data:[' + `${numbersInfected}` + ']}]}}'
+  
+  console.log(imageURL);
+  
+
+
+
+//   fetch(URL + '/' + `${$("#countries").val()}` + '/status/' + `${$("#dataType").val()}` + '/date/' + `${$("#afterDate").val()}` + 'T00:00:00Z')
+//   .then(response => response.json())
+//   .then(responseJsonCovidData => showGraph(responseJsonCovidData))
+// }
+
+// function showGraph(responseJsonCovidData) {
+//   console.log(responseJsonCovidData);
+
+//   let datesForGraphs = [];
+//   let dataSetsForGraph = [];
+//   const separateOutDatesAndDatasets = responseJsonCovidData.forEach(element => {
+//     datesForGraphs.push(element.Date);
+//     if ($("#dataType").val() === 'confirmed') {
+//       dataSetsForGraph.push(element.Confirmed);
+//     }
+//     else if ($("#dataType").val() === 'recovered') {
+//       dataSetsForGraph.push(element.Recovered);
+//     }
+//     else if ($("#dataType").val() === 'deaths') {
+//       dataSetsForGraph.push(element.Deaths);
+//     }
+//   }); 
 
     
-
-  //   fetch(URL + '/' + `${$("#countries").val()}` + '/status/' + `${$("#dataType").val()}` + '/date/' + `${$("#afterDate").val()}` + 'T00:00:00Z')
-  //   .then(response => response.json())
-  //   .then(responseJsonCovidData => showGraph(responseJsonCovidData))
-  // }
-
-  // function showGraph(responseJsonCovidData) {
-  //   console.log(responseJsonCovidData);
-
-  //   let datesForGraphs = [];
-  //   let dataSetsForGraph = [];
-  //   const separateOutDatesAndDatasets = responseJsonCovidData.forEach(element => {
-  //     datesForGraphs.push(element.Date);
-  //     if ($("#dataType").val() === 'confirmed') {
-  //       dataSetsForGraph.push(element.Confirmed);
-  //     }
-  //     else if ($("#dataType").val() === 'recovered') {
-  //       dataSetsForGraph.push(element.Recovered);
-  //     }
-  //     else if ($("#dataType").val() === 'deaths') {
-  //       dataSetsForGraph.push(element.Deaths);
-  //     }
-  //   }); 
-
-    
-  //   let finalDates = [];
-  //   function datesForGraphFormatted(arrayOfDates) {
-  //     (arrayOfDates).forEach(date => {
-  //       let slicedDate = date.slice(0, 10);
-  //       finalDates.push(`'${slicedDate}'`)
-  //     });
-  //   }
-  //   datesForGraphFormatted(datesForGraphs);
+//   let finalDates = [];
+//   function datesForGraphFormatted(arrayOfDates) {
+//     (arrayOfDates).forEach(date => {
+//       let slicedDate = date.slice(0, 10);
+//       finalDates.push(`'${slicedDate}'`)
+//     });
+//   }
+//   datesForGraphFormatted(datesForGraphs);
 
 
-  //   let finalNumbers = [];
-  //   function numbersOfCasesForGraphFormatted(arrayOfNumbersOfCases) {
-  //     arrayOfNumbersOfCases.forEach(numberOfCases => {
-  //       finalNumbers.push(`'${numberOfCases}'`)
-  //     });
-  //   }
-  //   numbersOfCasesForGraphFormatted(dataSetsForGraph);
+//   let finalNumbers = [];
+//   function numbersOfCasesForGraphFormatted(arrayOfNumbersOfCases) {
+//     arrayOfNumbersOfCases.forEach(numberOfCases => {
+//       finalNumbers.push(`'${numberOfCases}'`)
+//     });
+//   }
+//   numbersOfCasesForGraphFormatted(dataSetsForGraph);
 
    
 
-  //   let imageURL = `${graphURL + finalDates}], datasets:[{label:'${`${$("#dataType").val()}`}', data:[${finalNumbers}],fill:'false'}]}}`
+//   let imageURL = `${graphURL + finalDates}], datasets:[{label:'${`${$("#dataType").val()}`}', data:[${finalNumbers}],fill:'false'}]}}`
 
 
-  //   $('#graphDisplay').append('<img src="' + imageURL + '">');
-  //   $('#graphDisplay').show();
+//   $('#graphDisplay').append('<img src="' + imageURL + '">');
+//   $('#graphDisplay').show();
+
+
+}
 
  
 
-}
+
 
     
   
